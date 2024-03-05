f87ebbda567331b7b7c362777bc3cd734bb09fb1
foxdust42 <foxdust42@gmail.com>
2024-03-05 11:52:27 +0100

    binary printing

--- ls.asm
+++ ls.asm
@@ -1,4 +1,4 @@
-; rax, rdi ,rsi, rdx, r10, r9, r8 
+; rax, rdi ,rsi, rdx, rax, r9, r8 
 ; 0Ah -> linefeed
 ;
 
@@ -8,7 +8,8 @@ stat_struct RESB    88      ; 88  bytes
 print_buff  RESB    64       ; 4 bytes
 
 section .data
-msg1    db  'ls for:    '       ;
+msg1        db  'ls for:    '       ;
+nullchar    db  00h                 ;
 
 
 section .text
@@ -18,13 +19,13 @@ _start:
     mov     rax, msg1
     call    print
 
-
     mov     rax, buffer
 
     call    getwd
     call    printLF
     ;cwd on rax
 
+    ; stat cwd 
     mov     rdi, rax
     mov     rsi, stat_struct
     mov     rax, 4
@@ -33,9 +34,8 @@ _start:
     xor rax, rax
 
     mov rax, [stat_struct]
-    
-    add eax, 48
-    mov [print_buff], eax
+    add rax, 48
+    mov [print_buff], rax
     mov eax, 0Ah 
     mov [print_buff + 1], eax 
     mov eax, 00h
@@ -43,6 +43,11 @@ _start:
     
     mov rax, print_buff
     call print
+
+
+    mov     rax, 1001_0110b
+    call    print_bits
+
     ; exit /w code 0
     call    exit
 
@@ -112,3 +117,44 @@ getwd:
     pop     rsi
     pop     rdi
     ret
+
+; val on rax
+print_bits:
+    push    rdi
+    push    rsi
+    push    rax
+    mov     rdi , rax
+    mov     rsi, 1000_0000b
+    mov     rdx, 00b
+    mov     dword [print_buff + 8], 0h
+.pr_loop:
+    mov     rax, rdi
+    and     rax, rsi
+    cmp     rax, 0000b
+    jz      .zero
+    mov     rax, 0001b
+    jmp     .fin    
+    .zero:
+    mov     rax, 0000b
+    .fin:
+    add     rax, 48d
+    mov     [print_buff + rdx], rax
+    inc     rdx
+    shr     rsi, 0001b
+    cmp     rdx, 1000b
+    jnz     .pr_loop
+    mov     rax, print_buff
+    call    printLF
+
+    pop     rax
+    pop     rsi
+    pop     rdi
+    ret
+
+
+;statbuf in rax
+;statbuf:
+;   ull st_dev -> device id
+;   uchar   padding[4]
+stat:
+    
